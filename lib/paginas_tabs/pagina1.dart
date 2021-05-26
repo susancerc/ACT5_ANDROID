e3d7ad61766779e66051911e8c3f46c2411994a5
susancerc <71361469+susancerc@users.noreply.github.com>
2021-05-26 11:05:27 -0600

    Update pagina1.dart

--- lib/paginas_tabs/pagina1.dart
+++ lib/paginas_tabs/pagina1.dart
@@ -8,22 +8,22 @@ class PrimerTab extends StatelessWidget {
       body: Container(
         child: Center(
           child: Column(
-            // center the children
+            // center del children
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Icon(
                 Icons.receipt,
                 size: 200.0,
                 color: Colors.white,
-              ),
+              ),// fin de icon
               Text(
                 "Negocio",
                 style: TextStyle(color: Colors.white),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+              )//fin de text negocio
+            ],// fin de children widget
+          ),// fin child column
+        ),// fin center
+      ),//fin container
+    );// fin de scaffold
+  }//fin widget build
+}// fin class primertab
